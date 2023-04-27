585ca0eb687cae00af4f41dc20394796259479ae
russkyc <jrcc1023@gmail.com>
2023-04-27 16:16:07 +0800

    chore: add IAppInstance and IView interfaces

---
+++ GroomWise.Domain/Interfaces/IAppInstance.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (C) 2023 Russell Camo (Russkyc). - All Rights Reserved
+// 
+// Unauthorized copying or redistribution of all files, in source and binary forms via any medium
+// without written, signed consent from the author is strictly prohibited.
+
+using System.Collections.Generic;
+using GroomWise.Core.Models;
+
+namespace GroomWise.Core.Interfaces
+{
+    public interface IAppInstance
+    {
+        List<NavItem> NavItems { get; set; }
+    }
+}
