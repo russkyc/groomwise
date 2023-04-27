d7ba31054b825ba337b1eff7d3e85d34cc0e632f
russkyc <jrcc1023@gmail.com>
2023-04-27 14:14:19 +0800

    chore: add app pages

---
+++ GroomWise.Presentation/Views/Pages/PetsView.xaml.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (C) 2023 Russell Camo (Russkyc). - All Rights Reserved
+// 
+// Unauthorized copying or redistribution of all files, in source and binary forms via any medium
+// without written, signed consent from the author is strictly prohibited.
+
+using System.Windows.Controls;
+
+namespace Russkyc.GroomWise.Desktop.Views.Pages;
+
+public partial class PetsView : UserControl
+{
+    public PetsView()
+    {
+        InitializeComponent();
+    }
+}
