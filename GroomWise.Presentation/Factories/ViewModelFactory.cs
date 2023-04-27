49ac1bcfe9b3bd6d68f0ed15d46f409c5c4c915f
russkyc <jrcc1023@gmail.com>
2023-04-27 16:20:31 +0800

    feat: add ViwModelFactory

---
+++ GroomWise.Presentation/Factories/ViewModelFactory.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (C) 2023 Russell Camo (Russkyc). - All Rights Reserved
+// 
+// Unauthorized copying or redistribution of all files, in source and binary forms via any medium
+// without written, signed consent from the author is strictly prohibited.
+
+using GroomWise.Core.Interfaces;
+using Russkyc.GroomWise.Desktop.ViewModels;
+
+namespace Russkyc.GroomWise.Desktop.Factories;
+
+public static class ViewModelFactory
+{
+    public static MainViewModel CreateMainViewModel(IAppInstance app)
+    {
+        return new MainViewModel(app);
+    }
+}
