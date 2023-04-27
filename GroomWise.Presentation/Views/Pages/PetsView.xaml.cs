4d85df73a28459901dbdc12db1786f736b20a38b
russkyc <jrcc1023@gmail.com>
2023-04-27 16:18:53 +0800

    feat: add PetsView

--- GroomWise.Presentation/Views/Pages/PetsView.xaml.cs
+++ GroomWise.Presentation/Views/Pages/PetsView.xaml.cs
@@ -3,11 +3,11 @@
 // Unauthorized copying or redistribution of all files, in source and binary forms via any medium
 // without written, signed consent from the author is strictly prohibited.
 
-using System.Windows.Controls;
+using GroomWise.Core.Interfaces;
 
 namespace Russkyc.GroomWise.Desktop.Views.Pages;
 
-public partial class PetsView : UserControl
+public partial class PetsView : IView
 {
     public PetsView()
     {
