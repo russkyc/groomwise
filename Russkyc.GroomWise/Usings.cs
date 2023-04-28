26325b88845456859db53586d7914a84ff37d89a
russkyc <jrcc1023@gmail.com>
2023-04-28 23:00:33 +0800

    refactor(Usings): rearrange global usings for readability

--- Russkyc.GroomWise/Usings.cs
+++ Russkyc.GroomWise/Usings.cs
@@ -3,20 +3,27 @@
 // Unauthorized copying or redistribution of all files, in source and binary forms via any medium
 // without written, signed consent from the author is strictly prohibited.
 
+// DotNet
 global using System;
 global using System.Collections.Generic;
 
+// CommunityToolkit.Mvvm
 global using CommunityToolkit.Mvvm.ComponentModel;
 global using CommunityToolkit.Mvvm.Input;
 
+// Russkyc.DependencyInjection
 global using Russkyc.DependencyInjection.Implementations;
 global using Russkyc.DependencyInjection.Interfaces;
+
+// Russkyc.Abstractions
 global using Russkyc.Abstractions.Interfaces;
-global using Russkyc.GroomWise.Models.Entities;
-global using Russkyc.GroomWise.Models.Interfaces;
+
+// GroomWise
 global using Russkyc.GroomWise.Views;
 global using Russkyc.GroomWise.Views.Pages;
 global using Russkyc.GroomWise.ViewModels;
 global using Russkyc.GroomWise.Services;
-global using Russkyc.GroomWise.Models.Enums;
 global using Russkyc.GroomWise.Repositories;
+global using Russkyc.GroomWise.Models.Enums;
+global using Russkyc.GroomWise.Models.Entities;
+global using Russkyc.GroomWise.Models.Interfaces;
