b79a04d5697577739e01cd82d96b8cdadfb89773
russkyc <jrcc1023@gmail.com>
2023-04-28 22:45:50 +0800

    chore: optimize usings

--- Russkyc.GroomWise/Usings.cs
+++ Russkyc.GroomWise/Usings.cs
@@ -3,14 +3,20 @@
 // Unauthorized copying or redistribution of all files, in source and binary forms via any medium
 // without written, signed consent from the author is strictly prohibited.
 
+global using System;
+global using System.Collections.Generic;
+
 global using CommunityToolkit.Mvvm.ComponentModel;
 global using CommunityToolkit.Mvvm.Input;
 
 global using Russkyc.DependencyInjection.Implementations;
 global using Russkyc.DependencyInjection.Interfaces;
+global using Russkyc.Abstractions.Interfaces;
 global using Russkyc.GroomWise.Models.Entities;
 global using Russkyc.GroomWise.Models.Interfaces;
 global using Russkyc.GroomWise.Views;
 global using Russkyc.GroomWise.Views.Pages;
 global using Russkyc.GroomWise.ViewModels;
 global using Russkyc.GroomWise.Services;
+global using Russkyc.GroomWise.Models.Enums;
+global using Russkyc.GroomWise.Repositories;
