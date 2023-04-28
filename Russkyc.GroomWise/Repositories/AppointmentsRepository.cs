1f2acb631a3f16fe27cd36bd8603ffb8352f7da3
russkyc <jrcc1023@gmail.com>
2023-04-28 22:40:39 +0800

    feat: add appointments repository abstraction and implementation

---
+++ Russkyc.GroomWise/Repositories/AppointmentsRepository.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (C) 2023 Russell Camo (Russkyc). - All Rights Reserved
+// 
+// Unauthorized copying or redistribution of all files, in source and binary forms via any medium
+// without written, signed consent from the author is strictly prohibited.
+
+namespace Russkyc.GroomWise.Repositories;
+
+public class AppointmentsRepository : IAppointmentsRepository
+{
+    private IDatabaseService _databaseService;
+
+    public AppointmentsRepository(IDatabaseService databaseService)
+    {
+        _databaseService = databaseService;
+    }
+    
+    public bool Insert(IAppointment item)
+    {
+        return _databaseService.Insert(item);
+    }
+
+    public bool Insert(ICollection<IAppointment> items)
+    {
+        return _databaseService.Insert(items);
+    }
+
+    public IAppointment Get(Func<IAppointment, bool> filter)
+    {
+        return _databaseService.Get(filter);
+    }
+
+    public ICollection<IAppointment> GetAll(Func<IAppointment, bool> filter)
+    {
+        return _databaseService.GetAll(filter);
+    }
+
+    public ICollection<IAppointment> GetCollection()
+    {
+        return _databaseService.GetCollection<IAppointment>();
+    }
+
+    public bool Update(Func<IAppointment, bool> filter, Action<IAppointment> action)
+    {
+        return _databaseService.Update(filter, action);
+    }
+
+    public bool Delete(Func<IAppointment, bool> filter)
+    {
+        return _databaseService.Delete(filter);
+    }
+}
