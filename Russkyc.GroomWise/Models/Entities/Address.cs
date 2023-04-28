f03fc5511f0a3a1d22fbb5a09436bb39859cb104
russkyc <jrcc1023@gmail.com>
2023-04-28 22:31:36 +0800

    feat: add address entity type

---
+++ Russkyc.GroomWise/Models/Entities/Address.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (C) 2023 Russell Camo (Russkyc). - All Rights Reserved
+// 
+// Unauthorized copying or redistribution of all files, in source and binary forms via any medium
+// without written, signed consent from the author is strictly prohibited.
+
+namespace Russkyc.GroomWise.Models.Entities;
+
+public class Address : IAddress
+{
+    public string HouseNumber { get; set; }
+    public string Street { get; set; }
+    public string Barangay { get; set; }
+    public string City { get; set; }
+    public string Province { get; set; }
+    public string ZipCode { get; set; }
+}
