0bd39e5de455cbfc0843af36de60c655dcd407c1
russkyc <jrcc1023@gmail.com>
2023-04-28 22:40:09 +0800

    feat: add abstractions and placeholder implementation of database service

---
+++ Russkyc.GroomWise/Services/LiteDbDataService.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (C) 2023 Russell Camo (Russkyc). - All Rights Reserved
+// 
+// Unauthorized copying or redistribution of all files, in source and binary forms via any medium
+// without written, signed consent from the author is strictly prohibited.
+
+namespace Russkyc.GroomWise.Services;
+
+public class LiteDbDataService : IDatabaseService
+{
+    public bool Insert<T>(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Insert<T>(ICollection<T> item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T Get<T>(Func<T, bool> filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ICollection<T> GetAll<T>(Func<T, bool> filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ICollection<T> GetCollection<T>()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Update<T>(Func<T, bool> filter, Action<T> action)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Delete<T>(Func<T, bool> filter)
+    {
+        throw new NotImplementedException();
+    }
+}
