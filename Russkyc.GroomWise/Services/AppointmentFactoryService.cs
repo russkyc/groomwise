c7a4e4dab42a7463104fba47a8f2c9ef050411d7
russkyc <jrcc1023@gmail.com>
2023-04-28 22:37:31 +0800

    feat: add core entity factory services

---
+++ Russkyc.GroomWise/Services/AppointmentFactoryService.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (C) 2023 Russell Camo (Russkyc). - All Rights Reserved
+// 
+// Unauthorized copying or redistribution of all files, in source and binary forms via any medium
+// without written, signed consent from the author is strictly prohibited.
+
+namespace Russkyc.GroomWise.Services;
+
+public class AppointmentFactoryService : IAppointmentFactoryService
+{
+    public IAppointment Create()
+    {
+        return new Appointment();
+    }
+}
