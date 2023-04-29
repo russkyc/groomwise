307998688d3b6698c03deb89d4650b41e75c733b
russkyc <jrcc1023@gmail.com>
2023-04-29 15:26:50 +0800

    feat(DashboardViewModel): add notifications collection and dependency injection for notification factory service

--- Russkyc.GroomWise/ViewModels/DashboardViewModel.cs
+++ Russkyc.GroomWise/ViewModels/DashboardViewModel.cs
@@ -5,7 +5,37 @@
 
 namespace GroomWise.ViewModels;
 
-public class DashboardViewModel : IDashboardViewModel
+public partial class DashboardViewModel : ViewModelBase, IDashboardViewModel
 {
+    private readonly INotificationFactoryService _notificationFactoryService;
+
+    private object _collectionsLock;
     
+    [ObservableProperty]
+    private ObservableCollection<INotification> _notifications;
+
+    public DashboardViewModel(INotificationFactoryService notificationFactoryService)
+    {
+        _notificationFactoryService = notificationFactoryService;
+        _collectionsLock = new object();
+        Notifications = new ObservableCollection<INotification>();
+        BindingOperations.EnableCollectionSynchronization(Notifications,_collectionsLock);
+        GetNotifications();
+    }
+
+    void GetNotifications()
+    {
+        new Thread(
+            () =>
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    var item = _notificationFactoryService.Create();
+                    item.Title = $"Notification {i}";
+                    item.Description = $"Insert description for notification {i}";
+                    Notifications.Add(item);
+                    Thread.Sleep(2000);
+                }
+            }).Start();
+    }
 }
