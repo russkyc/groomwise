126f3fd4264b965f27210623992803236c7e542e
russkyc <jrcc1023@gmail.com>
2023-04-29 15:24:24 +0800

    feat: add notification entity, abstraction and temp layout
    - Add notification factory service abstraction and implementation

---
+++ Russkyc.GroomWise/Models/Entities/Notification.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (C) 2023 Russell Camo (Russkyc). - All Rights Reserved
+// 
+// Unauthorized copying or redistribution of all files, in source and binary forms via any medium
+// without written, signed consent from the author is strictly prohibited.
+
+namespace GroomWise.Models.Entities;
+
+public class Notification : INotification
+{
+    public object Icon { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+}
