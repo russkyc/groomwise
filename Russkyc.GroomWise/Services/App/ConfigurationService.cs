e3c54af8ef195d07d01f5f0e47c638ba146da945
russkyc <jrcc1023@gmail.com>
2023-05-02 02:21:03 +0800

    feat: add configuration service abstraction and implementation

---
+++ Russkyc.GroomWise/Services/App/ConfigurationService.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (C) 2023 Russell Camo (Russkyc). - All Rights Reserved
+// 
+// Unauthorized copying or redistribution of all files, in source and binary forms via any medium
+// without written, signed consent from the author is strictly prohibited.
+
+using Ini.Net;
+
+namespace GroomWise.Services.App;
+
+public class ConfigurationService : IConfigurationService
+{
+    public ConfigurationService()
+    {
+        Config = new IniFile("GroomWise.ini");
+    }
+    public IniFile Config { get; set; }
+}
