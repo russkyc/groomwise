03baf9902c406dbd8c1d419c09b87cd6b0050e99
russkyc <jrcc1023@gmail.com>
2023-05-02 02:31:12 +0800

    feat: add migration for accounts collection

---
+++ Russkyc.GroomWise/Services/Data/Migrations/AccountsMigration.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (C) 2023 Russell Camo (Russkyc). - All Rights Reserved
+// 
+// Unauthorized copying or redistribution of all files, in source and binary forms via any medium
+// without written, signed consent from the author is strictly prohibited.
+
+namespace GroomWise.Services.Data.Migrations;
+
+public class AccountsMigration : IDatabaseMigration
+{
+    public void Migrate()
+    {
+        BuilderServices.Resolve<IDatabaseService>().Add(
+            BuilderServices.Resolve<IAccountFactoryService>().Create(
+            "",
+            "",
+            "",
+            "admin@groomwise.com",
+            "admin",
+            "admin")
+            );
+    }
+}
