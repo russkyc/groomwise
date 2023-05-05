1fbfdc7808d1266ec525973d19dcda579d4cce63
russkyc <jrcc1023@gmail.com>
2023-05-05 17:06:06 +0800

    feat: add appointment schedule entity and factory

---
+++ Russkyc.GroomWise/Models/Entities/AppointmentSchedule.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (C) 2023 Russell Camo (Russkyc).- All Rights Reserved
+// 
+// Unauthorized copying or redistribution of all files, in source and binary forms via any medium
+// without written, signed consent from the author is strictly prohibited.
+
+namespace GroomWise.Models.Entities;
+
+public class AppointmentSchedule : IAppointmentSchedule
+{
+    public string? Day { get; set; }
+    public string? Time { get; set; }
+    public string? Date { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+}
---
+++ Russkyc.GroomWise/Models/Interfaces/Entity/IAppointmentSchedule.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (C) 2023 Russell Camo (Russkyc).- All Rights Reserved
+// 
+// Unauthorized copying or redistribution of all files, in source and binary forms via any medium
+// without written, signed consent from the author is strictly prohibited.
+
+namespace GroomWise.Models.Interfaces.Entity;
+
+public interface IAppointmentSchedule
+{
+    string? Day { get; set; }
+    string? Time { get; set; }
+    string? Date { get; set; }
+    string? Title { get; set; }
+    string? Description { get; set; }
+}
