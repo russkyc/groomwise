712cf6a628af4f9621aef20759856f9f704d9c64
russkyc <jrcc1023@gmail.com>
2023-05-06 01:41:00 +0800

    feat(AppointmentsViewModel): add appointment schedule factory injection, appointments initialization

--- Russkyc.GroomWise/ViewModels/AppointmentsViewModel.cs
+++ Russkyc.GroomWise/ViewModels/AppointmentsViewModel.cs
@@ -5,16 +5,45 @@
 
 namespace GroomWise.ViewModels;
 
-public class AppointmentsViewModel : ObservableObject, IAppointmentsViewModel
+public partial class AppointmentsViewModel : ObservableObject, IAppointmentsViewModel
 {
-    private IAppointmentFactoryService _appointmentFactory;
+    private IAppointmentScheduleFactory _appointmentScheduleFactoryService;
     private IAppointmentsRepositoryService _appointmentsRepositoryService;
+    private IAppointmentFactoryService _appointmentFactory;
 
+    [ObservableProperty]
+    private AppointmentsScheduleCollection _appointments;
     public AppointmentsViewModel(
         IAppointmentFactoryService appointmentFactory,
+        IAppointmentScheduleFactory appointmentScheduleFactory,
         IAppointmentsRepositoryService appointmentsRepositoryService)
     {
-        _appointmentFactory = appointmentFactory;
+        _appointmentScheduleFactoryService = appointmentScheduleFactory;
         _appointmentsRepositoryService = appointmentsRepositoryService;
+        _appointmentFactory = appointmentFactory;
+        
+        Appointments = new AppointmentsScheduleCollection();
+        
+        GetAppointments();
+    }
+
+    void GetAppointments()
+    {
+        Task.Run(
+            () =>
+            {
+                for (var i = 8; i < 12; i++)
+                {
+                    Appointments.Insert(
+                        0,
+                        _appointmentScheduleFactoryService.Create(
+                            "AM",
+                            $"{i}:00",
+                            $"{i}",
+                            $"Appointment {i}",
+                            $"Service scheduled for today"));
+                    Thread.Sleep(1000);
+                }
+            });
     }
 }
