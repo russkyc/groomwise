9292f9abb797802fc5ebb2f0f001e807433b3d0e
russkyc <jrcc1023@gmail.com>
2023-05-10 23:00:00 +0800

    build: add different app configs for build and debug
    - Renamed GroomWise.ini to appconfig.ini
    - Added appconfig-indev.ini for dev builds

--- Russkyc.GroomWise/Services/App/ConfigurationService.cs
+++ Russkyc.GroomWise/Services/App/ConfigurationService.cs
@@ -1,5 +1,5 @@
 ﻿// Copyright (C) 2023 Russell Camo (Russkyc).- All Rights Reserved
-// 
+//
 // Unauthorized copying or redistribution of all files, in source and binary forms via any medium
 // without written, signed consent from the author is strictly prohibited.
 
@@ -7,5 +7,9 @@ namespace GroomWise.Services.App;
 
 public class ConfigurationService : IConfigurationService
 {
-    public IniFile Config =>  new IniFile("GroomWise.ini");
+# if (DEBUG)
+    public IniFile Config => new IniFile("appconfig-indev.ini");
+# else
+    public IniFile Config => new IniFile("appconfig.ini");
+#endif
 }
