fdf600500a055e54ba8476fb5549572f03de3492
russkyc <jrcc1023@gmail.com>
2023-05-10 13:35:23 +0800

    refactor(ApplicationService): code cleanup, optimization

--- Russkyc.GroomWise/Services/App/ApplicationService.cs
+++ Russkyc.GroomWise/Services/App/ApplicationService.cs
@@ -1,5 +1,5 @@
 ﻿// Copyright (C) 2023 Russell Camo (Russkyc).- All Rights Reserved
-// 
+//
 // Unauthorized copying or redistribution of all files, in source and binary forms via any medium
 // without written, signed consent from the author is strictly prohibited.
 
@@ -7,31 +7,79 @@ namespace GroomWise.Services.App;
 
 public partial class ApplicationService : ObservableObject, IApplicationService
 {
-
+    private readonly IMaterialIconFactory _materialIconFactory;
     private readonly IConfigurationService _configurationService;
     private readonly ISessionManagerService _sessionManagerService;
-    private readonly INavItemFactoryService _navItemFactoryService;
-    private readonly IMaterialIconFactory _materialIconFactory;
-    
+    private readonly INavItemFactory _navItemFactory;
+
+    private List<INavItem> _nav;
+
     [ObservableProperty]
     private string? _appAuthor;
 
     [ObservableProperty]
     private string? _appVersion;
-    
+
     [ObservableProperty]
     private NavItemsCollection _navItems;
 
     public ApplicationService(
         ISessionManagerService sessionManagerService,
         IConfigurationService configurationService,
-        INavItemFactoryService navItemFactoryService,
-        IMaterialIconFactory materialIconFactory)
+        IMaterialIconFactory materialIconFactory,
+        INavItemFactory navItemFactory
+    )
     {
         _sessionManagerService = sessionManagerService;
         _configurationService = configurationService;
-        _navItemFactoryService = navItemFactoryService;
         _materialIconFactory = materialIconFactory;
+        _navItemFactory = navItemFactory;
+
+        _nav = new List<INavItem>
+        {
+            _navItemFactory.Create(
+                "Dashboard",
+                typeof(IDashboardView),
+                _materialIconFactory.Create(MaterialIconKind.ViewDashboard),
+                true,
+                new[] { EmployeeType.Admin, EmployeeType.Groomer, EmployeeType.Manager }
+            ),
+            _navItemFactory.Create(
+                "Appointments",
+                typeof(IAppointmentsView),
+                _materialIconFactory.Create(MaterialIconKind.EventAvailable),
+                false,
+                new[] { EmployeeType.Admin, EmployeeType.Groomer, EmployeeType.Manager }
+            ),
+            _navItemFactory.Create(
+                "Services",
+                typeof(IServicesView),
+                _materialIconFactory.Create(MaterialIconKind.BubbleChart),
+                false,
+                new[] { EmployeeType.Admin, EmployeeType.Manager }
+            ),
+            _navItemFactory.Create(
+                "Customers",
+                typeof(ICustomersView),
+                _materialIconFactory.Create(MaterialIconKind.People),
+                false,
+                new[] { EmployeeType.Admin, EmployeeType.Manager }
+            ),
+            _navItemFactory.Create(
+                "Pets",
+                typeof(IPetsView),
+                _materialIconFactory.Create(MaterialIconKind.Pets),
+                false,
+                new[] { EmployeeType.Admin, EmployeeType.Groomer, EmployeeType.Manager }
+            ),
+            _navItemFactory.Create(
+                "Reports",
+                typeof(IReportsView),
+                _materialIconFactory.Create(MaterialIconKind.BarChart),
+                false,
+                new[] { EmployeeType.Admin, EmployeeType.Manager }
+            )
+        };
         NavItems = new NavItemsCollection();
         BuildAppInfo();
     }
@@ -41,88 +89,21 @@ public partial class ApplicationService : ObservableObject, IApplicationService
         AppAuthor = "Russell Camo (@russkyc)";
         AppVersion = _configurationService.Config.ReadString("AppSettings", "Version");
     }
-    
+
     public void BuildNavItems()
     {
-        NavItems.Clear();
-        foreach (INavItem navItem in new[]
-                 {
-                     _navItemFactoryService.Create("Dashboard",
-                         typeof(IDashboardView),
-                         _materialIconFactory.Create(MaterialIconKind.ViewDashboard),
-                         true,
-                         new[]
-                         {
-                             AccountType.Admin,
-                             AccountType.Groomer,
-                             AccountType.Manager,
-                             AccountType.Customer
-                         }
-                     ),
-                     _navItemFactoryService.Create("Appointments",
-                         typeof(IAppointmentsView),
-                         _materialIconFactory.Create(MaterialIconKind.EventAvailable),
-                         false,
-                         new[]
-                         {
-                             AccountType.Admin,
-                             AccountType.Groomer,
-                             AccountType.Manager,
-                             AccountType.Customer
-                         }
-                     ),
-                     _navItemFactoryService.Create("Services",
-                         typeof(IServicesView),
-                         _materialIconFactory.Create(MaterialIconKind.BubbleChart),
-                         false,
-                         new[]
-                         {
-                             AccountType.Admin,
-                             AccountType.Manager
-                         }
-                     ),
-                     _navItemFactoryService.Create("Customers",
-                         typeof(ICustomersView),
-                         _materialIconFactory.Create(MaterialIconKind.People),
-                         false,
-                         new[]
-                         {
-                             AccountType.Admin,
-                             AccountType.Manager
-                         }
-                     ),
-                     _navItemFactoryService.Create("Pets",
-                         typeof(IPetsView),
-                         _materialIconFactory.Create(MaterialIconKind.BubbleChart),
-                         false,
-                         new[]
-                         {
-                             AccountType.Admin,
-                             AccountType.Groomer,
-                             AccountType.Manager,
-                             AccountType.Customer
-                         }
-                     ),
-                     _navItemFactoryService.Create("Reports",
-                         typeof(IReportsView),
-                         _materialIconFactory.Create(MaterialIconKind.BarChart),
-                         false,
-                         new[]
-                         {
-                             AccountType.Admin,
-                             AccountType.Manager
-                         }
-                     )
-                 })
-            
-            // Checks if nav item restriction allows access to current session type
-            if (_sessionManagerService != null
-                && _sessionManagerService.SessionUser?.Type != null
-                && navItem.AccountTypes!
-                    .ToList()
-                    .Contains((AccountType)_sessionManagerService.SessionUser?
-                        .Type!))
-                // Add nav item
-                NavItems.Add(navItem);
+        var currentUserType = (EmployeeType)_sessionManagerService.SessionUser!.EmployeeType!;
+        Application.Current.Dispatcher.BeginInvoke(() => NavItems.Clear());
+
+        _nav.Where(navItem => navItem.AccountTypes!.Contains(currentUserType))
+            .ToList()
+            .ForEach(navItem =>
+            {
+                navItem.Selected = navItem.Name == "Dashboard";
+                Application.Current.Dispatcher.InvokeAsync(() =>
+                {
+                    NavItems.Add(navItem);
+                });
+            });
     }
 }
