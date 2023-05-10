d6ead270ae1dd808aba163f51ffe82c03ad47d23
russkyc <jrcc1023@gmail.com>
2023-05-10 13:30:53 +0800

    feat: add async sqlite data service as IDataBaseServiceAsync implementation

--- Russkyc.GroomWise/Models/Collections/NavItemsCollection.cs
+++ Russkyc.GroomWise/Models/Collections/NavItemsCollection.cs
@@ -1,16 +1,11 @@
 ﻿// Copyright (C) 2023 Russell Camo (Russkyc).- All Rights Reserved
-// 
+//
 // Unauthorized copying or redistribution of all files, in source and binary forms via any medium
 // without written, signed consent from the author is strictly prohibited.
-
-using GroomWise.Models.Abstractions;
 
 namespace GroomWise.Models.Collections;
 
 /// <summary>
 /// <inheritdoc cref="SynchronizedObservableCollection{T}"/>
 /// </summary>
-public class NavItemsCollection : SynchronizedObservableCollection<INavItem>
-{
-    
-}
+public class NavItemsCollection : SynchronizedObservableCollection<INavItem> { }
