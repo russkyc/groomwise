55aee326a5af35c91296b5c261265a25b96d76dd
russkyc <jrcc1023@gmail.com>
2023-06-02 18:46:58 +0800

    feat(EmployeeRepository): add encryption service

--- Russkyc.GroomWise/Services/Repository/EmployeeRepository.cs
+++ Russkyc.GroomWise/Services/Repository/EmployeeRepository.cs
@@ -1,5 +1,5 @@
 ﻿// Copyright (C) 2023 Russell Camo (Russkyc).- All Rights Reserved
-// 
+//
 // Unauthorized copying or redistribution of all files, in source and binary forms via any medium
 // without written, signed consent from the author is strictly prohibited.
 
@@ -7,60 +7,53 @@ namespace GroomWise.Services.Repository;
 
 public class EmployeeRepository : IEmployeeRepository
 {
-
+    private readonly IEncryptionService _encryptionService;
     private IDatabaseServiceAsync _databaseService;
 
-    public EmployeeRepository(IDatabaseServiceAsync databaseService)
+    public EmployeeRepository(
+        IDatabaseServiceAsync databaseService,
+        IEncryptionService encryptionService
+    )
     {
         _databaseService = databaseService;
+        _encryptionService = encryptionService;
     }
 
     public bool Add(Employee item)
     {
-        return Task.Run(
-            async () => await _databaseService.Add(item)
-        ).GetAwaiter().GetResult();
+        return Task.Run(async () => await _databaseService.Add(item)).Result;
     }
 
     public bool AddMultiple(ICollection<Employee> items)
     {
-        return Task.Run(
-            async () => await _databaseService.AddMultiple(items)
-        ).GetAwaiter().GetResult();
+        return Task.Run(async () => await _databaseService.AddMultiple(items)).Result;
     }
 
     public Employee Get(Expression<Func<Employee, bool>> filter)
     {
-        return Task.Run(
-            async () => await _databaseService.Get(filter)
-        ).GetAwaiter().GetResult();
+        return Task.Run(async () => await _databaseService.Get(filter)).Result;
     }
 
     public ICollection<Employee> GetMultiple(Expression<Func<Employee, bool>> filter)
     {
-        return Task.Run(
-            async () => await _databaseService.GetMultiple(filter)
-        ).GetAwaiter().GetResult();
+        return Task.Run(async () => await _databaseService.GetMultiple(filter)).Result;
     }
 
     public ICollection<Employee> GetCollection()
     {
-        return Task.Run(
-            async () => await _databaseService.GetCollection<Employee>()
-        ).GetAwaiter().GetResult();
+        return Task.Run(async () => await _databaseService.GetCollection<Employee>()).Result;
     }
 
-    public bool Update(Expression<Func<Employee, bool>> filter, Expression<Func<Employee, Employee>> action)
+    public bool Update(
+        Expression<Func<Employee, bool>> filter,
+        Expression<Func<Employee, Employee>> action
+    )
     {
-        return Task.Run(
-            async () => await _databaseService.Update(filter, action)
-        ).GetAwaiter().GetResult();
+        return Task.Run(async () => await _databaseService.Update(filter, action)).Result;
     }
 
     public bool Delete(Expression<Func<Employee, bool>> filter)
     {
-        return Task.Run(
-            async () => await _databaseService.Delete(filter)
-        ).GetAwaiter().GetResult();
+        return Task.Run(async () => await _databaseService.Delete(filter)).Result;
     }
 }
