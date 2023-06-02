8ea5e12de3f42d68a516118251a46580d96e8bba
russkyc <jrcc1023@gmail.com>
2023-06-03 01:15:54 +0800

    feat: add migrations creation in cli

--- Russkyc.Groomwise.Cli/Program.cs
+++ Russkyc.Groomwise.Cli/Program.cs
@@ -3,9 +3,89 @@
 // Unauthorized copying or redistribution of all files, in source and binary forms via any medium
 // without written, signed consent from the author is strictly prohibited.
 
+using GroomWise.Models.Entities;
+using Russkyc.Groomwise.Cli.Services;
+
 namespace Russkyc.Groomwise.Cli;
 
 public class Program
 {
-    public static void Main(string[] args) { }
+    public static void Main(string[] args)
+    {
+        CreateAccountsMigration();
+        CreateEmployeesMigration();
+    }
+
+    static void CreateAccountsMigration()
+    {
+        new Migration<Account>(
+            "Accounts.json",
+            "Accounts",
+            new List<Account>
+            {
+                new Account
+                {
+                    Id = 0,
+                    Username = "russkyc",
+                    Password = "1234",
+                    Email = "russkyc@groomwise.com",
+                    EmployeeId = 0
+                },
+                new Account
+                {
+                    Id = 1,
+                    Username = "manager",
+                    Password = "manager",
+                    Email = "manager@groomwise.com",
+                    EmployeeId = 1
+                },
+                new Account
+                {
+                    Id = 2,
+                    Username = "groomer",
+                    Password = "groomer",
+                    Email = "groomer@groomwise.com",
+                    EmployeeId = 2
+                },
+            }
+        ).Create();
+    }
+
+    static void CreateEmployeesMigration()
+    {
+        new Migration<Employee>(
+            "Employees.json",
+            "Employees",
+            new List<Employee>
+            {
+                new Employee
+                {
+                    Id = 0,
+                    FirstName = "John Russell",
+                    MiddleName = "Casabuena",
+                    LastName = "Camo",
+                    AddressId = 0,
+                    EmployeeType = 0
+                },
+                new Employee
+                {
+                    Id = 1,
+                    FirstName = "John",
+                    MiddleName = "Williams",
+                    LastName = "Doe",
+                    AddressId = 1,
+                    EmployeeType = 1
+                },
+                new Employee
+                {
+                    Id = 2,
+                    FirstName = "Karen",
+                    MiddleName = "Karen",
+                    LastName = "Karen",
+                    AddressId = 2,
+                    EmployeeType = 2
+                }
+            }
+        ).Create();
+    }
 }
---
+++ Russkyc.Groomwise.Cli/Services/Migration.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (C) 2023 Russell Camo (Russkyc).- All Rights Reserved
+//
+// Unauthorized copying or redistribution of all files, in source and binary forms via any medium
+// without written, signed consent from the author is strictly prohibited.
+
+using JsonFlatFileDataStore;
+
+namespace Russkyc.Groomwise.Cli.Services;
+
+public class Migration<T>
+    where T : class
+{
+    private const string MIGRATIONS_PATH = "Migrations";
+    private readonly string _path;
+    private readonly string _name;
+    private List<T> _collection;
+
+    public Migration(string path, string name, List<T> collection)
+    {
+        _collection = collection;
+        _name = name;
+        _path = path;
+
+        if (!Directory.Exists(MIGRATIONS_PATH))
+            Directory.CreateDirectory(MIGRATIONS_PATH);
+    }
+
+    public void Create()
+    {
+        var store = new DataStore($"{MIGRATIONS_PATH}/{_path}");
+        store.GetCollection<T>(_name).InsertMany(_collection.AsEnumerable());
+    }
+}
