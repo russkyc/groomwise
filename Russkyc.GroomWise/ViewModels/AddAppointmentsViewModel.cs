7eba5bd5460c0432be77b4cd0973f4d4cde3afe7
russkyc <jrcc1023@gmail.com>
2023-06-05 12:40:32 +0800

    feat: add addappointments view and viewmodel

---
+++ Russkyc.GroomWise/ViewModels/AddAppointmentsViewModel.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (C) 2023 Russell Camo (Russkyc).- All Rights Reserved
+//
+// Unauthorized copying or redistribution of all files, in source and binary forms via any medium
+// without written, signed consent from the author is strictly prohibited.
+
+namespace GroomWise.ViewModels;
+
+public partial class AddAppointmentsViewModel : ViewModelBase, IAddAppointmentsViewModel
+{
+    private readonly IAppointmentsRepository _appointmentsRepository;
+
+    [ObservableProperty]
+    private Appointment _newAppointment;
+
+    public AddAppointmentsViewModel(IAppointmentsRepository appointmentsRepository)
+    {
+        _appointmentsRepository = appointmentsRepository;
+    }
+
+    [RelayCommand]
+    void AddAppointment()
+    {
+        _appointmentsRepository.Add(NewAppointment);
+    }
+}
