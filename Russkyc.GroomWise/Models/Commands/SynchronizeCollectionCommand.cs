7743fec1ed9bb13d918bb71e745af21f07bb83a8
russkyc <jrcc1023@gmail.com>
2023-06-05 12:06:14 +0800

    feat: add collection synchronization command

---
+++ Russkyc.GroomWise/Models/Commands/SynchronizeCollectionCommand.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (C) 2023 Russell Camo (Russkyc).- All Rights Reserved
+//
+// Unauthorized copying or redistribution of all files, in source and binary forms via any medium
+// without written, signed consent from the author is strictly prohibited.
+
+namespace GroomWise.Models.Commands;
+
+public class SynchronizeCollectionCommand<T, TCollection> : ICommand
+    where TCollection : SynchronizedObservableCollection<T>
+    where T : IEntity
+{
+    public bool CanExecute { get; private set; }
+    private readonly TCollection _target;
+    private readonly ICollection<T> _source;
+
+    public SynchronizeCollectionCommand(ref TCollection target, ICollection<T> source)
+    {
+        _target = target;
+        _source = source;
+        CanExecute = true;
+    }
+
+    public void Execute()
+    {
+        if (CanExecute)
+        {
+            _target.RemoveRange(_target.Except(_source));
+            _target.AddRange(
+                _source.Where(
+                    sourceItem => !(_target.Any(targetItem => targetItem.Id == sourceItem.Id))
+                )
+            );
+        }
+    }
+}
