7af8c197b2b5be5992e4fa86ffdf1c84c6fce0f1
russkyc <jrcc1023@gmail.com>
2023-06-07 00:25:08 +0800

    feat: move connection source provider to models

--- Russkyc.GroomWise/Models/Data/ConnectionSourceProvider.cs
+++ Russkyc.GroomWise/Models/Data/ConnectionSourceProvider.cs
@@ -3,7 +3,7 @@
 // Unauthorized copying or redistribution of all files, in source and binary forms via any medium
 // without written, signed consent from the author is strictly prohibited.
 
-namespace GroomWise.Services.Data;
+namespace GroomWise.Models.Data;
 
 public class ConnectionSourceProvider : IConnectionSourceProvider
 {
