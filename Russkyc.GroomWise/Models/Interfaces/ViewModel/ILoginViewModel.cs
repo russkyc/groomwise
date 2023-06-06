260ee342a420392d5c4110d094750fde1fa19b29
russkyc <jrcc1023@gmail.com>
2023-06-07 00:33:00 +0800

    refactor: update login implementation, code cleanup

--- Russkyc.GroomWise/Models/Interfaces/ViewModel/ILoginViewModel.cs
+++ Russkyc.GroomWise/Models/Interfaces/ViewModel/ILoginViewModel.cs
@@ -1,5 +1,5 @@
 ﻿// Copyright (C) 2023 Russell Camo (Russkyc).- All Rights Reserved
-// 
+//
 // Unauthorized copying or redistribution of all files, in source and binary forms via any medium
 // without written, signed consent from the author is strictly prohibited.
 
@@ -7,4 +7,15 @@ namespace GroomWise.Models.Interfaces.ViewModel;
 
 public interface ILoginViewModel
 {
+    /// <inheritdoc cref="LoginViewModel._applicationService"/>
+    IApplicationService ApplicationService { get; set; }
+
+    /// <inheritdoc cref="LoginViewModel._notifications"/>
+    NotificationsCollection Notifications { get; set; }
+
+    /// <inheritdoc cref="LoginViewModel._password"/>
+    string? Password { get; set; }
+
+    /// <inheritdoc cref="LoginViewModel._username"/>
+    string? Username { get; set; }
 }
