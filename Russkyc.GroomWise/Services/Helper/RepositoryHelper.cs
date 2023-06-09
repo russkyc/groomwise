bbe12a85910236e4d5cc54ea34d278f2034f13e0
russkyc <jrcc1023@gmail.com>
2023-06-09 19:33:03 +0800

    feat: add repository and value helpers

---
+++ Russkyc.GroomWise/Services/Helper/RepositoryHelper.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (C) 2023 Russell Camo (Russkyc).- All Rights Reserved
+//
+// Unauthorized copying or redistribution of all files, in source and binary forms via any medium
+// without written, signed consent from the author is strictly prohibited.
+
+namespace GroomWise.Services.Helper;
+
+public static class RepositoryHelper
+{
+    public static int GetLastId<T>(this Repository<T> repository)
+        where T : class, IEquatable<T>, IEntity, new()
+    {
+        return repository.GetAll().OrderByDescending(saved => saved.Id).First().Id;
+    }
+
+    public static int CountAll<T>(this Repository<T> repository)
+        where T : class, IEquatable<T>, IEntity, new()
+    {
+        return repository.GetAll().Count();
+    }
+
+    public static int CountWhere<T>(this Repository<T> repository, Predicate<T> filter)
+        where T : class, IEquatable<T>, IEntity, new()
+    {
+        return repository.FindAll(filter).Count();
+    }
+}
