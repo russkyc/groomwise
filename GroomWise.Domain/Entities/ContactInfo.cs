df5bc50697a6637526427d3ea0b982ed0ee35438
russkyc <jrcc1023@gmail.com>
2023-07-15 13:33:54 +0800

    chore: add new entities and enums
    - Added new entities (GroomingService, Notification, Role, Session, ContactInfo, Employee) and enums (NotificationType, AppointmentStatus).
    
    Signed-off-by: russkyc <jrcc1023@gmail.com>

---
+++ GroomWise.Domain/Entities/ContactInfo.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (C) 2023 Russell Camo (Russkyc).- All Rights Reserved
+//
+// Unauthorized copying or redistribution of all files, in source and binary forms via any medium
+// without written, signed consent from the author is strictly prohibited.
+
+using GroomWise.Domain.Interfaces;
+
+namespace GroomWise.Domain.Entities;
+
+public record ContactInfo : IEntity
+{
+    public Guid Id { get; set; }
+    public string? ContactNumber { get; set; }
+    public string? Telephone { get; set; }
+    public string? Email { get; set; }
+}
