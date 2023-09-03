a281ff35f32f751bd98ab931649f9f99c027cbdf
Russell Camo <32549126+russkyc@users.noreply.github.com>
2023-09-04 01:00:42 +0800

    chore: update licensing
    Signed-off-by: Russell Camo <32549126+russkyc@users.noreply.github.com>

--- GroomWise.WPF/Views/Templates/CustomerInfoCardTemplate.xaml.cs
+++ GroomWise.WPF/Views/Templates/CustomerInfoCardTemplate.xaml.cs
@@ -1,7 +1,13 @@
-﻿// Copyright (C) 2023 Russell Camo (Russkyc).- All Rights Reserved
+﻿// GroomWise
+// Copyright (C) 2023  John Russell C. Camo (@russkyc)
 // 
-// Unauthorized copying or redistribution of all files, in source and binary forms via any medium
-// without written, signed consent from the author is strictly prohibited.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY
 
 using System.Windows.Controls;
 
