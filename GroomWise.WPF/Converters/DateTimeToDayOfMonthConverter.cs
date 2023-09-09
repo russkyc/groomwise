841d0d691c74c86e2f6bb7ba87d763fefd69b15a
Russell Camo <32549126+russkyc@users.noreply.github.com>
2023-09-09 10:38:38 +0800

    fix: appointment date is now showing correct day (timezone fixed)
    Signed-off-by: Russell Camo <32549126+russkyc@users.noreply.github.com>

--- GroomWise.WPF/Converters/DateTimeToDayOfMonthConverter.cs
+++ GroomWise.WPF/Converters/DateTimeToDayOfMonthConverter.cs
@@ -1,11 +1,11 @@
 ﻿// GroomWise
 // Copyright (C) 2023  John Russell C. Camo (@russkyc)
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY
 
@@ -15,18 +15,17 @@ using System.Windows.Data;
 
 namespace GroomWise.Converters;
 
-[ValueConversion(typeof(DateTime),typeof(string))]
+[ValueConversion(typeof(DateTime), typeof(string))]
 public class DateTimeToDayOfMonthConverter : IValueConverter
 {
+    public static DateTimeToDayOfMonthConverter Instance = new();
 
-    public static DateTimeToDayOfMonthConverter Instance = new DateTimeToDayOfMonthConverter();
-    
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        var date = value is DateTime
-            ? (DateTime)value : default;
-        return date.Day
-            .ToString();
+        var date = value is DateTime ? (DateTime)value : default;
+        var timeZone = TimeZoneInfo.Local;
+        var timeZoneDate = TimeZoneInfo.ConvertTime(date, timeZone);
+        return timeZoneDate.Day.ToString();
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
