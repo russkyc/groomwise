b77f7af0995d2b2e8d06c15e71468038cc78dfc2
Russell Camo <32549126+russkyc@users.noreply.github.com>
2023-09-11 16:23:29 +0800

    fix: fix navigation
    Signed-off-by: Russell Camo <32549126+russkyc@users.noreply.github.com>

--- GroomWise.Infrastructure/Navigation/NavigationService.cs
+++ GroomWise.Infrastructure/Navigation/NavigationService.cs
@@ -1,11 +1,11 @@
 ﻿// GroomWise
 // Copyright (C) 2023  John Russell C. Camo (@russkyc)
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY
 
@@ -17,57 +17,66 @@ namespace GroomWise.Infrastructure.Navigation;
 [RegisterSingleton<INavigationService, NavigationService>]
 public class NavigationService : INavigationService
 {
+    private readonly Dictionary<Enum, object> _views;
+    private readonly object _lock;
+    
     private SynchronizationContext? _synchronizationContext;
-    private Dictionary<Enum, object?>? _views;
-    private static IWindow? _currentWindow;
-    private static readonly object Lock = new();
+    private IWindow? _currentWindow;
+
+    public NavigationService()
+    {
+        _lock = new();
+        lock (_lock)
+        {
+            _views = new Dictionary<Enum, object>();
+        }
+    }
 
     public IWindow CurrentWindow
     {
-        get
-        {
-            lock (Lock)
-            {
-                return _currentWindow!;
-            }
-        }
+        get { return _currentWindow!; }
     }
 
     public void Add(Enum key, IWindow instance)
     {
-        _views?.Add(key, instance);
+        lock (_lock)
+        {
+            _views.Add(key, instance);
+        }
     }
 
     public void Navigate(Enum key, bool hidePrevious = true)
     {
-        _synchronizationContext?.Send(
-            _ =>
+        lock (_lock)
+        {
+            var view = _views[key];
+            if (view is IWindow window)
             {
-                var view = _views?[key];
-                if (view is IWindow window)
-                {
-                    window.Show();
-                    lock (Lock)
+                window.Show();
+                _synchronizationContext?.Send(
+                    _ =>
                     {
                         if (hidePrevious)
                         {
                             _currentWindow?.Hide();
                         }
                         _currentWindow = window;
-                    }
-                }
-            },
-            null
-        );
+                    },
+                    null
+                );
+            }
+        }
     }
 
     public void Initialize(SynchronizationContext? context, IWindow? mainWindow)
     {
         if (context is not null)
         {
-            _synchronizationContext = context;
-            _views = new();
-            _currentWindow = mainWindow;
+            lock (_lock)
+            {
+                _synchronizationContext = context;
+                _currentWindow = mainWindow;
+            }
         }
     }
 }
